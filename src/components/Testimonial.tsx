348206037bdca49d70a8a48b8ae3050d28704eec
james <james500nju@gmail.com>
2025-01-03 11:32:14 +0300

    `Refactor Testimonial component: update layout and add quotes`

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -40,10 +40,7 @@ export function Testimonial({
 }: TestimonialProps) {
 
   return (
-    <div className="relative flex flex-col items-center p-6 outline-none">
-  {/* Opening quote */}
-  <span className="absolute top-[4] left-[] ml-[-30rem] text-4xl sm:text-5xl md:text-6xl text-primary -translate-x-1/2 -translate-y-1/2">&#x275D;</span>
-
+    <div className="flex flex-col items-center p-6 outline-none">
   {/* Stars */}
   <div className="flex mb-4">
     {[...Array(5)].map((_, i) => (
@@ -62,14 +59,18 @@ export function Testimonial({
       objectFit="cover"
     />
   </div>
-  {/* Description */}
-  <p className="text-gray-600 dark:text-white text-center mb-4" dangerouslySetInnerHTML={{ __html: name }}></p>
-  <p className="text-gray-600 dark:text-white text-center mb-4" dangerouslySetInnerHTML={{ __html: description }}></p>
+  {/* Description with quotes */}
+  <div className="relative w-full">
+    {/* Opening quote */}
+    <span className="absolute text-primary400 top-0 left-0 ml-[3rem] text-4xl sm:text-5xl md:text-6xl text-primary -translate-x-1/2 -translate-y-1/2">&#x275D;</span>
+    <p className="text-gray-600 dark:text-white text-center mb-4 px-8" dangerouslySetInnerHTML={{ __html: name }}></p>
+    <p className="text-gray-600 dark:text-white text-center mb-4 px-8" dangerouslySetInnerHTML={{ __html: description }}></p>
+    
+    {/* Closing quote */}
+    <span className="absolute bottom-0 right-0 text-4xl sm:text-5xl md:text-6xl translate-x-1/2 translate-y-1/2 text-primary400">&#x275E;</span>
+  </div>
   {/* Username */}
   <p className="font-semibold text-primary300" dangerouslySetInnerHTML={{ __html: onBehalfOf }}></p>
-
-  {/* Closing quote */}
-  <span className="absolute bottom-[0] text-red-600 mr-[-38rem] right-[8] text-4xl sm:text-5xl md:text-6xl text-secondary translate-x-1/2 translate-y-1/2">&#x275E;</span>
 </div>
   );
 }
